d086874135787cb0477b24fc28b308bc543d95de
Nikki DelRosso <nikk3r+github@gmail.com>
2020-05-25 14:01:52 -0700

    Update stock-price.user.js

--- stock-price.user.js
+++ stock-price.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         Neopets: Display Stock Price
 // @namespace    https://nikkidelrosso.com
-// @version      0.1
+// @version      0.1.1
 // @description  Displays the price of Neopets stocks on the purchase page, and pre-fill the number of shares to max.
 // @author       Nikki DelRosso
 // @include      http*://www.neopets.com/stockmarket.phtml?type=buy*
 // @include      http*://www.neopets.com/stockmarket.phtml?ticker=*&type=buy*
 // @grant        none
-// @updateURL    https://github.com/Nikker/Neopets-Userscripts/raw/master/stock-price.user.js
 // ==/UserScript==
 
 (function() {
@@ -49,6 +48,16 @@
         let ticker = purchaseForm.ticker_symbol.value;
         let tickerEntry = getTickerEntry(ticker);
 
+        if (!ticker) {
+            div.innerHTML = "";
+            div.style.display = "none";
+            return;
+        } else {
+            div.style.display = "";
+        }
+
+        div.title = "";
+
         if (!tickerEntry) {
             div.innerHTML = `<small>⚠️<em>Can't find ${ticker}</em></small>`;
             div.style.color = 'red';
@@ -59,15 +68,18 @@
         if (tickerEntry.price < minBuyPrice) {
             div.style.color = 'red';
             emoji = '⛔';
+            div.title = "This stock is less than the minimum purchase price - you can't buy it";
         } else if (tickerEntry.price > minBuyPrice) {
             div.style.color = 'orange';
             emoji = '⚠️';
+            div.title = "This stock costs more than the minimum purchase price";
         } else {
             div.style.color = 'green';
             emoji = '❇️';
+            div.title = "Nice! This stock is the same as the minimum purchase price.";
         }
 
-        div.innerHTML = `<b>${emoji} Cost: ${tickerEntry.price} NP</b> ${emoji}<br><small><em>For ${ticker} on page load</em></small>`;
+        div.innerHTML = `<b>${emoji}Cost: ${tickerEntry.price} NP</b> ${emoji}<br><small><em>For ${ticker} on page load</em></small>`;
         return true;
     }
 
