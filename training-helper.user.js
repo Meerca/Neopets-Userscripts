57e355f351b9096a78d93c92124bb3f53e375564
Devin Elrose <hiddnist@gmail.com>
2024-07-28 21:10:05 -0700

    Updates version number

--- training-helper.user.js
+++ training-helper.user.js
@@ -2,7 +2,7 @@
 // @name         Neopets Training Helper
 // @author       Hiddenist
 // @namespace    https://hiddenist.com
-// @version      2024-07-24
+// @version      2024-07-28
 // @description  Makes codestone training your pet require fewer clicks and less math.
 // @match        http*://www.neopets.com/island/fight_training.phtml?type=status
 // @match        http*://www.neopets.com/island/training.phtml?type=status
